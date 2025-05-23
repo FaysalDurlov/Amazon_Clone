274f331a2df740bc00312fd993446bf49da18b28
FaysalDurlov <foysaldurlov@gmail.com>
2025-05-23 19:05:26 +0600

    Image_product_Object_included_css_Added_

---
+++ Amazon Project/scripts/amazon.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+        priceCent: 1090,
+        rating: {
+            stars: 4.5,
+            count: 87
+        }
+    },
+    {
+        name: 'Intermediate Size Basketball',
+        image: 'images/products/intermediate-composite-basketball.jpg',
+        priceCent: 2095,
+        rating: {
+            stars: 4,
+            count: 127
+        }
+    },
+    {
+        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        priceCent: 799,
+        rating: {
+            stars: 4.5,
+            count: 56
+        }
+    },
+
+];
