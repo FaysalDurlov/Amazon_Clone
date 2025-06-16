313bd12110cb0b32128808ece6374cd1a51c29f7
FaysalDurlov <foysaldurlov@gmail.com>
2025-06-16 23:30:53 +0600

    tesing_funciton_call_count_using_Jasmine

--- Amazon Project/tests_Jasmine/data/cartTest.js
+++ Amazon Project/tests_Jasmine/data/cartTest.js
@@ -5,11 +5,17 @@ describe('test suite: addToCart',()=>{
 
     });
     it('add a New Product to the Cart',()=>{
+        spyOn(localStorage,"setItem");
+
+
+
         spyOn(localStorage,"getItem").and.callFake(()=>{
             return JSON.stringify([]);
         });
         loadFromStorage();
         addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
         expect(cart.length).toEqual(1);
+        
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     });
 });
