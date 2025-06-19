34ed39ea2c477edd34067517aebd85f114479b24
FaysalDurlov <foysaldurlov@gmail.com>
2025-06-19 17:16:06 +0600

    Jasmine_testing_Function_Completed

--- Amazon Project/tests_Jasmine/data/cartTest.js
+++ Amazon Project/tests_Jasmine/data/cartTest.js
@@ -2,19 +2,33 @@ import { cart, addToCart, loadFromStorage } from "../../data/cart.js";
 
 describe('test suite: addToCart',()=>{
     it('add an exiting product to the cart',()=>{
+        spyOn(localStorage,"getItem").and.callFake(()=>{
+            return JSON.stringify([{
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                Quantity: 1,
+                deliveryOptionsId: '1'
+            }]); // faking/ Mocking value
+        });
+        loadFromStorage(); // Reloading this again cz we want to see the faked/mocked version
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].Quantity).toEqual(2);
 
     });
     it('add a New Product to the Cart',()=>{
-        spyOn(localStorage,"setItem");
+        spyOn(localStorage,"setItem"); // initialing spying to see in future what this setItem method does.
 
         spyOn(localStorage,"getItem").and.callFake(()=>{
-            return JSON.stringify([]);
+            return JSON.stringify([]); // faking/ Mocking value
         });
-        loadFromStorage();
+        loadFromStorage(); // Reloading this again cz we want to see the faked/mocked version
         addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
         expect(cart.length).toEqual(1);
         
-        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1); 
+        // checking how many times setItem method has been called (remember we initialize spying on set item?)
+
         expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
         expect(cart[0].Quantity).toEqual(1);
     });
