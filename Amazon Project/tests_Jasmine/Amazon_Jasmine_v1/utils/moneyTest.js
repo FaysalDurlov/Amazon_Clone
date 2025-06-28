fb1b8a07a4042884048e3223e36d513d711e11c8
FaysalDurlov <foysaldurlov@gmail.com>
2025-06-28 23:18:45 +0600

    Using_jasmine_Tested_moneyJs_files_code

---
+++ Amazon Project/tests_Jasmine/Amazon_Jasmine_v1/utils/moneyTest.js
@@ -0,0 +1,7 @@
+import {formatCurrency} from "../../../scripts/utils/money.js";
+
+describe("This Is a Suit Which is Used to Give a title/ Discribe My Test. Ex. (Money format testing.) ",()=>{
+    it("Under the describe: these are subSection. Or My Test Case for a funtion which Can be Mutiple. Ex. (Moeney formate is 20.95)",()=>{
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+});
