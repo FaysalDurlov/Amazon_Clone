6a6708c3be5ba9e44ab5acf01113a3d13efd7a45
FaysalDurlov <foysaldurlov@gmail.com>
2025-06-28 23:28:10 +0600

    addeed_multiple_test_for_moneyJs_structured_cartJs_in_jasmine_v1

--- Amazon Project/tests_Jasmine/Amazon_Jasmine_v1/utils/moneyTest.js
+++ Amazon Project/tests_Jasmine/Amazon_Jasmine_v1/utils/moneyTest.js
@@ -4,4 +4,10 @@ describe("This Is a Suit Which is Used to Give a title/ Discribe My Test. Ex. (M
     it("Under the describe: these are subSection. Or My Test Case for a funtion which Can be Mutiple. Ex. (Moeney formate is 20.95)",()=>{
         expect(formatCurrency(2095)).toEqual('20.95');
     });
+    it("For input 0",()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it("For Input 2000.5",()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    })
 });
