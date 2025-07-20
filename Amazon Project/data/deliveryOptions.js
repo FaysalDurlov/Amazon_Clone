dff4241b6a8071af3500cb594fa26de4feb39479
FaysalDurlov <foysaldurlov@gmail.com>
2025-07-20 23:56:44 +0600

    Added_new_test_for_orderSummary

--- Amazon Project/data/deliveryOptions.js
+++ Amazon Project/data/deliveryOptions.js
@@ -1,3 +1,4 @@
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 export const deliveryOptions = [{
   id: '1',
   deliveryDays: 7,
---
+++ Amazon Project/tests_Jasmine/Amazon_Jasmine_v1/Checkout/orderSummaryTest.js
@@ -0,0 +1,26 @@
+import {renderOrderSummary} from "../../../scripts/checkout/orderSummary.js";
+import {loadFromStorage} from "../../../data/cart.js";
+
+describe("Test Suit: Order Summary",()=>{
+    it("Displays The Cart",()=>{
+        document.querySelector('.js-class-container').innerHTML = `<div class="js_order_summary"></div>`;
+
+        spyOn(localStorage,"getItem").and.callFake(()=>{
+            return JSON.stringify([
+                {
+                  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                  Quantity: 2,
+                  deliveryOptionsId: '1'
+                },
+                {
+                  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                  Quantity: 1,
+                  deliveryOptionsId: '2'
+                }
+              ]);
+        });
+        loadFromStorage();
+        renderOrderSummary();
+    })
+
+})
